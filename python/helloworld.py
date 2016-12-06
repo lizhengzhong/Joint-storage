1201b4403787973db775c04bc234054427c35873
lizhengzhong <1004413482@qq.com>
2016-12-06 17:02:01 +0800

    python

---
+++ python/helloworld.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+name = raw_input('please enter your name:')
+print 'hello', name
+
+
---
+++ python/integer.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python2.7
+# print absolute value of an integer
+
+value = -20
+if value >= 0:
+	print value
+else:
+	print -value
